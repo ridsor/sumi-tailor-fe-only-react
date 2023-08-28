109d3982f17392037045df5eefc77fa55f4c30c1
ridsor <ridsorgamerz@gmail.com>
2023-08-28 17:51:25 +0700

    mengubah warna kombinasi

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className="absolute top-0 left-0 right-0 z-50">
       <div className="container px-2 py-4">
-        <article className="bg-[#0f0f0f] w-full flex items-center text-white p-2.5 rounded-full lg:justify-between relative">
+        <article className="bg-two w-full flex items-center text-white p-2.5 rounded-full lg:justify-between relative">
           <div className="flex items-center order-1 px-2 left gap-x-1">
             <FaCircle className="fill-white" size="1rem" />
             <span className="text-base font-bold">Sumi Tailor</span>
@@ -16,7 +16,8 @@ const Header = () => {
           <div className="order-3 center lg:order-2">
             <button
               className="block p-2 mx-2 lg:hidden"
-              onClick={() => setHamburger((prev) => (prev ? false : true))}>
+              onClick={() => setHamburger((prev) => (prev ? false : true))}
+            >
               <FaBars
                 className={`${
                   hamburger ? "fill-gray-400" : "fill-white"
@@ -29,8 +30,9 @@ const Header = () => {
                 hamburger
                   ? " opacity-100 pointer-events-auto right-0"
                   : "-right-1/3 pointer-events-none opacity-0 lg:pointer-events-auto lg:opacity-100"
-              } transition-all top-[110%] lg:block lg:static absolute bg-white lg:bg-inherit shadow-md lg:shadow-none p-4 lg:p-0 w-full max-w-[250px] lg:w-auto lg:max-w-none lg:rounded-none rounded-md lg:border-none border`}>
-              <ul className="flex gap-x-10 text-[#0f0f0f] lg:text-gray-400 flex-col lg:flex-row gap-y-3">
+              } transition-all top-[110%] lg:block lg:static absolute bg-white lg:bg-inherit shadow-md lg:shadow-none p-4 lg:p-0 w-full max-w-[250px] lg:w-auto lg:max-w-none lg:rounded-none rounded-md lg:border-none border`}
+            >
+              <ul className="flex gap-x-10 text-[#0f0f0f] lg:text-white flex-col lg:flex-row gap-y-3">
                 <li>
                   <Link to="/">Home</Link>
                 </li>
@@ -50,8 +52,8 @@ const Header = () => {
             </nav>
           </div>
           <div className="order-2 ml-auto right lg:order-3 lg:ml-0">
-            <Link to="/login" className="bg-[#2a28f0] p-3 rounded-full block">
-              <FaUser className="fill-white" size="1.2rem" />
+            <Link to="/login" className="bg-white p-3 rounded-full block">
+              <FaUser className="fill-two" size="1.2rem" />
             </Link>
           </div>
         </article>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,83 +3,91 @@ import { useState } from "react";
 const backendURL = import.meta.env.BACKEND_URL;
 
 interface InputsLogin {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
 const index = () => {
-	const [inputsLogin, setInputsLogin] = useState<InputsLogin>({
-		email: '',
-		password: ''
-	});
-	const [nodeValidation, setNodeValidation] = useState<React.ReactNode | undefined>(undefined);
+  const [inputsLogin, setInputsLogin] = useState<InputsLogin>({
+    email: "",
+    password: "",
+  });
+  const [nodeValidation, setNodeValidation] = useState<
+    React.ReactNode | undefined
+  >();
 
-	const handleSubmitFormLogin = async (e:React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+  const handleSubmitFormLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-		if(handleValidationLogin()) return;
+    if (handleValidationLogin()) return;
 
-		const data = await axios.post(backendURL+'/api/auth/login',inputsLogin).then(res => res.data);
-		if(data.status !== 'success') console.error(data.message);
-	}
+    const data = await axios
+      .post(backendURL + "/api/auth/login", inputsLogin)
+      .then((res) => res.data);
+    if (data.status !== "success") console.error(data.message);
+  };
 
-	const handleValidationLogin = () => {
-		setNodeValidation(undefined);
+  const handleValidationLogin = () => {
+    setNodeValidation(undefined);
 
-		const listValidation = [];
-		var result = false;
-		if(!inputsLogin.email) {
-			listValidation.unshift('Email tidak boleh kosong.');
-			result = true;
-		}
-		
-		var rs = inputsLogin.email;
-		var atps=rs.indexOf("@");
-		var dots=rs.lastIndexOf(".");
-		if (atps<1 || dots<atps+2 || dots+2>=rs.length) {
-			listValidation.unshift('Alamat email tidak valid.');
-			result = true;
-		}
+    const listValidation = [];
+    var result = false;
+    if (!inputsLogin.email) {
+      listValidation.unshift("Email tidak boleh kosong.");
+      result = true;
+    }
 
-		if(!inputsLogin.password) {
-			listValidation.unshift('Password tidak boleh kosong.');
-			result = true;
-		}
+    var rs = inputsLogin.email;
+    var atps = rs.indexOf("@");
+    var dots = rs.lastIndexOf(".");
+    if (atps < 1 || dots < atps + 2 || dots + 2 >= rs.length) {
+      listValidation.unshift("Alamat email tidak valid.");
+      result = true;
+    }
 
-		if(listValidation.length > 0) {
-			setNodeValidation(
-				<div>
-					<h5 className="text-red-500 text-center">Terdapat kesalahan yang terjadi</h5>
-					<ul className="[list-style:inherit]">
-						{listValidation.map((x,i) => (
-							<li key={i} className="text-center">{x}</li>
-						))}
-					</ul>
-				</div>
-			);
-		}
+    if (!inputsLogin.password) {
+      listValidation.unshift("Password tidak boleh kosong.");
+      result = true;
+    }
 
-		return result;
-	}
+    if (listValidation.length > 0) {
+      setNodeValidation(
+        <div>
+          <h5 className="text-red-500 text-center">
+            Terdapat kesalahan yang terjadi
+          </h5>
+          <ul className="[list-style:inherit]">
+            {listValidation.map((x, i) => (
+              <li key={i} className="text-center">
+                {x}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
 
-	const handleChangeInputLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setInputsLogin(prev => ({...prev, [e.target.name]: e.target.value}));
-	}
+    return result;
+  };
+
+  const handleChangeInputLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputsLogin((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
   return (
-	<>
-	<section className="pt-36 pb-16">
-		<div className="container">
-			{(nodeValidation ? nodeValidation : '')}
+    <>
+      <section className="pt-36 pb-16 bg-three">
+        <div className="container">
+          {/* {(nodeValidation ? nodeValidation : '')}
 			<form onSubmit={handleSubmitFormLogin} className="flex flex-col gap-3 w-full lg:w-1/2 mx-auto mt-4">
 				<input type="text" name="email" className="border" onChange={handleChangeInputLogin} />
 				<input type="text" name="password" className="border" onChange={handleChangeInputLogin} />
 				<button className="border bg-black text-white p-3">Login</button>
-			</form>
-		</div>
-	</section>
-	</>
-  )
-}
+			</form> */}
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default index
+export default index;
