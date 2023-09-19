c7a1ef5aec0aa315d7a9e9f12305295e0c7cb30e
ridsor <ridsorgamerz@gmail.com>
2023-09-19 10:25:46 +0700

    mengubah struktur file layouts

--- src/components/Layouts/Main.tsx
+++ src/components/Layouts/Main.tsx
@@ -2,54 +2,52 @@ import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import { useEffect, useState } from "react";
-import {routes} from '../../router'
-import AuthMain from "../AuthLayouts/AuthMain";
+import { routes } from "../../router";
+import AuthMain from "./AuthLayouts/AuthMain";
 
 const Main = () => {
   const location = useLocation();
-  const [layouts, setLayouts] = useState<string>('main');
+  const [layouts, setLayouts] = useState<string>("main");
 
   useEffect(() => {
-    if(layouts === 'main') {
+    if (layouts === "main") {
       document.body.classList.add("overflow-x-hidden");
     }
   }, []);
 
-  useEffect(()=>{
-    for(const route of routes.main) {
-      if('/'+route === location.pathname) {
-        if(layouts === 'main') break;
-        setLayouts('main')
-        break
+  useEffect(() => {
+    for (const route of routes.main) {
+      if ("/" + route === location.pathname) {
+        if (layouts === "main") break;
+        setLayouts("main");
+        break;
       }
     }
 
-    for(const route of routes.auth) {
-      if('/'+route === location.pathname) {
-        if(layouts === 'auth') break;
-        setLayouts('auth')
-        break
+    for (const route of routes.auth) {
+      if ("/" + route === location.pathname) {
+        if (layouts === "auth") break;
+        setLayouts("auth");
+        break;
       }
     }
-  },[location]);
+  }, [location]);
 
-  switch(layouts) {
-    case 'main':
+  switch (layouts) {
+    case "main":
       return (
         <>
-          <Header/>
+          <Header />
           <main>
-            <Outlet/>
+            <Outlet />
           </main>
-          <Footer/>
+          <Footer />
         </>
-      )
-    case 'auth':
-      return <AuthMain/>
+      );
+    case "auth":
+      return <AuthMain />;
     default:
-      return (
-        <>404</>
-      )
+      return <>404</>;
   }
 };
 
